72a54b6e4859bbe7b777412c552d6e4dea29eaec
VitaliiShevchenko <iksel@ukr.net>
2024-12-29 22:09:55 +0200

    Remove unused tests and enhance AdminUser model validations
    Deleted obsolete test files to clean up the codebase as they were no longer in use. Updated the AdminUser model to include validations for name, email, and password to enforce stricter data integrity constraints. Also disabled comments in ActiveAdmin configuration to streamline functionality.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,9 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  MAX_NAME_LENGTH = 100
+  validates :name, length: { maximum: MAX_NAME_LENGTH }, presence: true, default: ""
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: MAX_NAME_LENGTH }
+  validates :password, length: { minimum: 6 }, presence: true
 end
