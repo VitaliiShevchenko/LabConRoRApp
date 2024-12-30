643dd731c07424e43ec3302f8c5c2bb2fb71d2ea
VitaliiShevchenko <iksel@ukr.net>
2024-12-30 14:47:48 +0200

    Add migrations for suppliers and materials tables
    Introduced two new database tables: `suppliers` and `materials`. The `suppliers` table includes fields for contact and product details, while the `materials` table tracks material details with a reference to suppliers. These changes establish a foundational structure for supplier and material management.

---
+++ db/migrate/20241230124501_create_supplier.rb
@@ -0,0 +1,15 @@
+class CreateSupplier < ActiveRecord::Migration[7.2]
+  def change
+    create_table :suppliers do |t|
+      t.string :name
+      t.string :mobile
+      t.string :email
+      t.string :website
+      t.string :contact_person
+      t.string :note
+      t.string :product_list
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20241230124631_create_material.rb
@@ -0,0 +1,15 @@
+class CreateMaterial < ActiveRecord::Migration[7.2]
+  def change
+    create_table :materials do |t|
+      t.datetime :date
+      t.string :name
+      t.string :desc
+      t.references :supplier, null: false, foreign_key: true
+      t.float :price
+      t.string :note
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
