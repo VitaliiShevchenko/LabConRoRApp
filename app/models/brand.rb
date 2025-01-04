90e7662b3af9b6ddd136c8b459ccc822faca5693
VitaliiShevchenko <iksel@ukr.net>
2025-01-04 23:36:49 +0200

    Added ransackable_atrributes to admin model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,12 @@
 class Brand < ApplicationRecord
   belongs_to :creator
   has_many :brand_receipts
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "creator_id", "updated_at", "name", "desc", "intended_use", "calculated_cost"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[creator brand_receipts]
+  end
 end
