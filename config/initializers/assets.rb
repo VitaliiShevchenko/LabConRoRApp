22a21e77d854d062c8a1695803d04e14c59dba52
VitaliiShevchenko <iksel@ukr.net>
2025-01-04 23:51:39 +0200

    Setup precompile files in the /config/initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,10 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.precompile += %w[
+  jquery.min.js
+  jquery-ui.js
+  bootstrap.min.js
+  popper.js
+]
