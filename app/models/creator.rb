66ccc1daab387181658991f642f13c879dd328b5
VitaliiShevchenko <iksel@ukr.net>
2025-01-04 23:36:19 +0200

    Added ransackable_atrributes to creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,4 +2,16 @@ class Creator < ApplicationRecord
   belongs_to :user
   has_many   :brands
   has_many   :tests
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true, uniqueness: true
+
+  def self.ransackable_associations(auth_object = nil)
+    ["brands", "tests", "user"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "first_name", "id", "last_name", "order_description", "phone", "position", "updated_at", "user_id"]
+  end
+
 end
