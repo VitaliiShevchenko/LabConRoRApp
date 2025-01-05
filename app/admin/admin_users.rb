dfd2b062e68782746cb2d4c24367943d0b5b9858
VitaliiShevchenko <iksel@ukr.net>
2025-01-05 19:29:04 +0200

    LC-46 Created functional in the AdminActive for Users and Creators

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :name, :password, :password_confirmation
 
   index do
     selectable_column
@@ -12,19 +12,19 @@ ActiveAdmin.register AdminUser do
     actions
   end
 
-  filter :email
-  filter :name
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
   form do |f|
     f.inputs do
       f.input :email
+      f.input :name
       f.input :password
       f.input :password_confirmation
     end
     f.actions
   end
 
+  filter :email
+  filter :name
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
 end
--- app/admin/creators.rb
+++ app/admin/creators.rb
@@ -1,35 +1,58 @@
-
 ActiveAdmin.register Creator do
-  permit_params :first_name,
+  permit_params :user_id,
+                :first_name,
                 :last_name,
                 :phone,
-                :position
+                :position,
+                :order_description
+
   config.batch_actions = true
-
-  config.action_items.delete_if { |item| item.name == :new }
-  config.action_items.delete_if { |item| item.name == :edit }
-  config.action_items.delete_if { |item| item.name == :destroy }
-
-  actions :all, except: []
-
-  # action_item :index, only: :index do
-  #   if current_admin_user
-  #     link_to 'Create Person', send("new_#{activeadminpath}_person_path")
-  #   end
-  # end
 
   form title: 'Creator info' do |f|
     div class: 'scroll-y' do
       f.inputs do
         if current_admin_user
+          f.input :user_id,  as: :hidden, input_html: { value: User.last.id }
+          f.input :user_id if f.object.persisted?
           f.input 'first_name'
           f.input 'last_name'
           f.input 'phone'
+          f.input 'position'
+          f.input 'order_description'
         end
-        #f.input :community_ids, as: :check_boxes, collection: Community.all
       end
     end
     f.actions
+  end
+
+  controller do
+    def creator_params
+      params.require(:creator).permit(:user_id, :position, :first_name, :last_name, :phone, :order_description)
+    end
+    def create
+      @creator = Creator.new(creator_params)
+
+      if @creator.save
+        flash[:notice] = "Creator created successfully!"
+        redirect_to admin_creators_path
+      else
+        flash[:error] = "Error:  #{@creator.errors.full_messages.join(", ")}. " \
+          "To start, the User must register. You must send them a registration request. After that, you can create a Creator. " \
+          "Check Users!"
+        render :new
+      end
+    end
+
+    def update
+      begin
+        Creator.update(creator_params)
+        flash[:notice] = "Creator updated successfully!"
+        redirect_to admin_creators_path
+      rescue ActiveRecord::RecordInvalid => e
+        flash[:error] = "Error: #{e}. "
+        render :new
+      end
+    end
   end
 
   filter :first_name, as: :string
