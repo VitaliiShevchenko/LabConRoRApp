fe305f112beb30166c9791bc26afaebd3cca0907
VitaliiShevchenko <iksel@ukr.net>
2025-01-06 15:32:04 +0200

    LC-5 Methods was added to Session Controller
    configure_sign_in_params
    verify_existence_creator

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
+  before_action :verify_existence_creator, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,7 +22,13 @@ class Users::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def verify_existence_creator
+    creator = Creator.find_by(user_id: User.where(email: sign_in_params[:email]).first&.id)
+    flash[:notice] = "Creator not found" if creator.nil?
+    redirect_to new_user_session_path    if creator.nil?
+  end
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email ])
+  end
 end
