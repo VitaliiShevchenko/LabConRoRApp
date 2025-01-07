b9a5f61eb469fd85b98b1b8b395c7b073cd1a0fd
VitaliiShevchenko <iksel@ukr.net>
2025-01-07 20:36:32 +0200

    LC-5 Changed creator helper.rb in according reviewer's note      Changed user.rb concerning rubocop's notes

--- app/helpers/creator_helper.rb
+++ app/helpers/creator_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module CreatorHelper
-  def is_creator?(user_email)
-    !Creator.find_by(user_id: User.where(email: user_email).first&.id).nil?
+  def is_creator?(email)
+    @user = User.find_by(email:)
+    !@user&.creator.nil?
   end
 end
