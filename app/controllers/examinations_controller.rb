406ab0a1cde99a13b063287e6b05c865cf623dda
VitaliiShevchenko <iksel@ukr.net>
2025-01-09 10:06:26 +0200

    LC-6 Added sample variable to the examinations#index      Added form and form.text_field to the new.html.erb

--- app/controllers/examinations_controller.rb
+++ app/controllers/examinations_controller.rb
@@ -2,6 +2,10 @@ class ExaminationsController < ApplicationController
   # before_action :set_examination, only: [:show, :edit, :update, :destroy]
 
   def index
+    @test = Test.new
+    @test.created_at = Date.today
+    @test.total_time = 600
+    @test.brand_receipt_id = "XLF A6540BL"
     render :new
   end
 end
