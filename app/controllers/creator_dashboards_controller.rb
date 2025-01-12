43ebf967d381abd4f7b3fdd9b43437608cb566d0
VitaliiShevchenko <iksel@ukr.net>
2025-01-12 17:29:32 +0200

    LC-8 Refactoring      Created new controller BRANDS and VIEWS      Has been moved necessary files & methods      Added new routes

--- app/controllers/creator_dashboards_controller.rb
+++ app/controllers/creator_dashboards_controller.rb
@@ -4,28 +4,6 @@ class CreatorDashboardsController < ApplicationController
     render :new
   end
 
-  def brands
-    @brands = Brand.all
-    @status_new_rec = false
-    render :brands
-  end
-
-  def create_brand
-    brand = Brand.create(permit_params)
-    if brand.errors.any?
-      flash[:alert] = brand.errors.full_messages.first
-    else
-      @brands = Brand.all
-      render :brands
-    end
-  end
-
-  def find_brand
-    fnd = "%#{params[:find]}%"
-    @brands = Brand.where("name LIKE ? OR intended_use LIKE ? OR description LIKE ?", fnd, fnd, fnd).order(:name)
-    render :brands
-  end
-
   def brand_receipts
     render :brand_receipts
   end
@@ -44,22 +22,5 @@ class CreatorDashboardsController < ApplicationController
 
   def tests
     render :tests
-  end
-
-  def new_record
-    @brands = Brand.all
-    @status_new_rec = true
-    @brand = Brand.new
-    @brand.creator_id = current_user.creator.id
-    render :brands
-  end
-
-  private
-  def is_new_record?
-    @new_record ||= false
-  end
-
-  def permit_params
-    params.require(:brand).permit([ :name, :intended_use, :description, :calculated_cost, :creator_id ])
   end
 end
