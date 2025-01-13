2112c42b85cf32ade94a44516af88b8d6046531b
VitaliiShevchenko <iksel@ukr.net>
2025-01-13 19:51:47 +0200

    LC-9 Created BrandReceiptsController.      Make index.html.erb in the bran_receipts directories.      Added some styles to the main.scss

--- app/controllers/brand_receipts_controller.rb
+++ app/controllers/brand_receipts_controller.rb
@@ -1,2 +1,29 @@
 class BrandReceiptsController < ApplicationController
+
+  def index
+    @brand = Brand.last
+    @brand_receipts = @brand.brand_receipts.order(receipt_num: :asc)
+    @status_new_rec = false
+    render :index
+  end
+
+  def find
+    @brand = Brand.find(params[:id])
+    @brand_receipts = @brand.brand_receipts.order(receipt_num: :asc)
+    render :index
+  end
+
+  def create
+    brand_receipt= BrandReceipt.create(permit_params)
+    if brand_receipt.errors.any?
+      flash[:alert] = brand_receipt.errors.full_messages.first
+    else
+      @brand_receipts = BrandReceipt.all
+      render :index
+    end
+  end
+
+  def permit_params
+    params.require(:brand_receipt).permit([ :density, :hardnessShA, :hardnessShD, :receipt_num, :brand_id ])
+  end
 end
