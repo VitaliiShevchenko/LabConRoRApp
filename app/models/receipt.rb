15da3b67768a122a2b592378824e6484f7dbebf8
VitaliiShevchenko <iksel@ukr.net>
2025-01-19 10:54:07 +0200

    LC-12  Created new method: join_mt_sp_names. This method create array of material and supplier united names by material id.      Added method to the _output_readonly_fields shim.      Created tests.

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,10 @@
 class Receipt < ApplicationRecord
   belongs_to :brand_receipt
   belongs_to :material
+
+  def join_mt_sp_names
+    material = Material.find(self.material_id)
+    supplier = material.supplier
+    "#{material.name} | #{supplier.name}"
+  end
 end
--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Receipt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#get_material_supplier_str" do
+    let(:supplier) { Supplier.create name: 'Supplier Name', active: nil, id: 1 }
+    let(:material) { Material.create name: 'Material',  description: 'Material Description',   price: 10.00,
+                                     note: 'Material Note', supplier_id: nil, id: 1, available: nil }
+    let(:receipt) { Receipt.create material_id: material.id, id: 1 }
+
+    before(:each) do
+      supplier.save
+    end
+    context 'when material_id is exist' do
+      it "displays special double name" do
+        material[:supplier_id] = supplier[:id]
+        material.save
+        expect(receipt.join_mt_sp_names).to eq "#{material.name} | #{supplier.name}"
+      end
+    end
+
+    context "when material_id is nil" do
+      it "return nil" do
+        receipt[:material_id] = nil
+        expect(receipt.join_mt_sp_names ).to eq nil
+      end
+    end
+  end
 end
