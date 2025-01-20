0a2f2fbae4b5953a1f6b960d861d27115aa9ddb9
VitaliiShevchenko <iksel@ukr.net>
2025-01-20 21:01:05 +0200

    LC-12 Added new field to the BrandReceipt page.          Moved up receipt_num field.          Made migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_17_094737) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_20_182228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_17_094737) do
     t.integer "receipt_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "calculated_cost"
     t.index ["brand_id"], name: "index_brand_receipts_on_brand_id"
   end
 
