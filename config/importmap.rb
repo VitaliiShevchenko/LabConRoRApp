8ad6144cdae5ab78225ffe228039d91ed3e4b5c6
VitaliiShevchenko <iksel@ukr.net>
2025-01-27 23:59:05 +0200

    LC-14 Customized importmap for Chart.js and ActionCable

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,7 @@ pin "jquery", to: "jquery.js", preload: true
 pin "jquery_ui", to: "jquery-ui.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
 pin "@poppers/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
