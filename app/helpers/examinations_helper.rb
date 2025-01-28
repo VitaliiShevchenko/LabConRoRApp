a1cd0b2f3431a28d3e99649a12b8d2aec16b15ad
VitaliiShevchenko <iksel@ukr.net>
2025-01-28 11:28:37 +0200

    LC-14 Create consistent_data_points in the examinations_helper.rb

--- app/helpers/examinations_helper.rb
+++ app/helpers/examinations_helper.rb
@@ -1,2 +1,17 @@
 module ExaminationsHelper
+  def consistent_data_points(records)
+    [
+      records.pluck(:torque),
+      records.pluck(:speed),
+      records.pluck(:mold_temp),
+      records.pluck(:material_temp),
+      records.pluck(:voltage),
+      records.pluck(:current),
+      records.pluck(:frequency),
+      records.pluck(:power),
+      records.pluck(:energy),
+      records.pluck(:alarm),
+      records.pluck(:testing_time)
+    ]
+  end
 end
