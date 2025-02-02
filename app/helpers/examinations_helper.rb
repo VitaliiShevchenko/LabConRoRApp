d6a4177783f7827a537d622a0401f6b3d2e95daf
VitaliiShevchenko <iksel@ukr.net>
2025-02-02 23:46:49 +0200

    LC-14 Added ability of the data interpolation

--- app/helpers/examinations_helper.rb
+++ app/helpers/examinations_helper.rb
@@ -1,17 +1,69 @@
 module ExaminationsHelper
-  def consistent_data_points(records)
-    [
-      records.pluck(:torque),
-      records.pluck(:speed),
-      records.pluck(:mold_temp),
-      records.pluck(:material_temp),
-      records.pluck(:voltage),
-      records.pluck(:current),
-      records.pluck(:frequency),
-      records.pluck(:power),
-      records.pluck(:energy),
-      records.pluck(:alarm),
-      records.pluck(:testing_time)
-    ]
+  def consistent_data_points(test_id)
+    begin
+      records = DataFlow.where(test_id:).order(:testing_time)
+      {
+        dataPoints:
+          [
+          records.pluck(:torque),
+          records.pluck(:speed),
+          records.pluck(:mold_temp),
+          records.pluck(:material_temp),
+          records.pluck(:voltage),
+          records.pluck(:current),
+          records.pluck(:frequency),
+          records.pluck(:power),
+          records.pluck(:energy),
+          records.pluck(:alarm),
+          records.pluck(:testing_time)
+        ],
+        averageValues:
+          [
+            records.average(:voltage),
+            records.average(:current),
+            records.average(:frequency),
+            records.average(:power),
+            records.average(:energy)
+          ]
+      }
+
+    rescue ActiveRecord::RecordNotFound
+      [ [] ]
+    end
+  end
+
+  def consistent_data_by_x_points(test_id, points)
+    begin
+      records = DataFlow.where(test_id:).order(:testing_time)
+      torque = records.pluck(:torque)
+      avg_torque = torque.each_cons(points).map { |group| group.sum.to_f / points }
+      {
+        dataPoints:
+          [
+            avg_torque,
+            records.pluck(:speed),
+            records.pluck(:mold_temp),
+            records.pluck(:material_temp),
+            records.pluck(:voltage),
+            records.pluck(:current),
+            records.pluck(:frequency),
+            records.pluck(:power),
+            records.pluck(:energy),
+            records.pluck(:alarm),
+            records.pluck(:testing_time)
+          ],
+        averageValues:
+          [
+            records.average(:voltage),
+            records.average(:current),
+            records.average(:frequency),
+            records.average(:power),
+            records.average(:energy)
+          ]
+      }
+
+    rescue ActiveRecord::RecordNotFound
+      [ [] ]
+    end
   end
 end
